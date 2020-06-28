32099a80ea2fe128fe570d8ffb4d98062cfedc8d
fcarducci <fcarducci20@gmail.com>
2020-06-28 19:45:31 +0200

    testing

--- src/Controller/Maleteo/MaleteoController.php
+++ src/Controller/Maleteo/MaleteoController.php
@@ -21,7 +21,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-
 class MaleteoController extends AbstractController
 {
 /**
